7eec71dec5231f9b1d5fd54730b5981062b37c4c
DKlaver <dklaver15@gmail.com>
2021-03-15 20:57:20 -0500

    Added app.use string for replies.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ connectDB();
 app.use(express.json());
 app.use(cors());
 app.use("/api/comments", comments);
+app.use("/api/replies", comments);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
