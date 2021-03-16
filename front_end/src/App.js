83c3b2449dc448feb858b557036776d30ec8611d
Lee Stratman <stratmanlc10@gmail.com>
2021-03-16 18:14:02 -0500

    Add application shell to App component.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,5 +1,109 @@
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <header className="pb-24 bg-indigo-600">
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="relative py-5 flex items-center justify-center lg:justify-between">
+            <div className="flex-1 min-w-0 px-12 lg:hidden">
+              <div className="max-w-xs w-full mx-auto">
+                <label for="search" className="sr-only">
+                  Search
+                </label>
+                <div className="relative text-white focus-within:text-gray-600">
+                  <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                    <svg
+                      className="h-5 w-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <input
+                    id="search"
+                    className="block w-full bg-white bg-opacity-20 py-2 pl-10 pr-3 border border-transparent rounded-md leading-5 text-gray-900 placeholder-white focus:outline-none focus:bg-opacity-100 focus:border-transparent focus:placeholder-gray-500 focus:ring-0 sm:text-sm"
+                    placeholder="Search"
+                    type="search"
+                    name="search"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="hidden lg:block border-t border-white border-opacity-20 py-5">
+            <div className="grid grid-cols-3 gap-8 items-center">
+              <div className="col-start-3">
+                <div className="max-w-md w-full mx-auto">
+                  <label for="search" className="sr-only">
+                    Search
+                  </label>
+                  <div className="relative text-white focus-within:text-gray-600">
+                    <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                      <svg
+                        className="h-5 w-5"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </div>
+                    <input
+                      id="search"
+                      className="block w-full bg-white bg-opacity-20 py-2 pl-10 pr-3 border border-transparent rounded-md leading-5 text-gray-900 placeholder-white focus:outline-none focus:bg-opacity-100 focus:border-transparent focus:placeholder-gray-500 focus:ring-0 sm:text-sm"
+                      placeholder="Search"
+                      type="search"
+                      name="search"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="-mt-24 pb-8">
+        <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h1 className="sr-only">Page title</h1>
+
+          <div className="grid grid-cols-1 gap-4 items-start lg:grid-cols-3 lg:gap-8">
+            <div className="grid grid-cols-1 gap-4 lg:col-span-2">
+              <section aria-labelledby="section-1-title">
+                <h2 className="sr-only" id="section-1-title">
+                  Section title
+                </h2>
+                <div className="rounded-lg bg-white overflow-hidden shadow">
+                  <div className="p-6"></div>
+                </div>
+              </section>
+            </div>
+
+            <div className="grid grid-cols-1 gap-4">
+              <section aria-labelledby="section-2-title">
+                <h2 className="sr-only" id="section-2-title">
+                  Section title
+                </h2>
+                <div className="rounded-lg bg-white overflow-hidden shadow">
+                  <div className="p-6"></div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
