c7c19bd5f09f51b88bbd0b3cb94f909891833399
Lee Stratman <stratmanlc10@gmail.com>
2021-03-17 20:36:48 -0500

    Add CommentList and comment component.

---
+++ front_end/src/Components/Comment.js
@@ -0,0 +1,101 @@
+import React from "react";
+
+const Comment = ({ comment }) => (
+  <li className="list-none px-4 py-4 sm:px-6 rounded-lg bg-white shadow">
+    <article aria-labelledby="question-title-41150">
+      <div>
+        <div className="flex space-x-3">
+          <div className="flex-shrink-0">
+            <svg
+              className="h-10 w-10"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </div>
+          <div className="min-w-0 flex-1">
+            <p className="text-sm font-medium text-gray-900">Anonymous</p>
+            <p className="text-sm text-gray-500">
+              {comment.date.split("T")[0]}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="mt-2 text-sm text-gray-700 space-y-4">
+        <p>{comment.text}</p>
+      </div>
+      <div className="mt-6 flex justify-between space-x-8">
+        <div className="flex space-x-6">
+          <span className="inline-flex items-center text-sm">
+            <button className="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+              <svg
+                className="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+              </svg>
+              <span className="font-medium text-gray-900">{comment.likes}</span>
+              <span className="sr-only">likes</span>
+            </button>
+          </span>
+          <span className="inline-flex items-center text-sm">
+            <button className="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+              <svg
+                className="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 14H5.236a2 2 0 01-1.789-2.894l3.5-7A2 2 0 018.736 3h4.018a2 2 0 01.485.06l3.76.94m-7 10v5a2 2 0 002 2h.096c.5 0 .905-.405.905-.904 0-.715.211-1.413.608-2.008L17 13V4m-7 10h2m5-10h2a2 2 0 012 2v6a2 2 0 01-2 2h-2.5"
+                />
+              </svg>
+              <span className="font-medium text-gray-900">
+                {comment.dislikes}
+              </span>
+              <span className="sr-only">dislikes</span>
+            </button>
+          </span>
+          <span className="inline-flex items-center text-sm">
+            <button className="inline-flex space-x-2 text-gray-400 hover:text-gray-500">
+              <svg
+                className="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <span className="font-medium text-gray-900">
+                {comment.replies.length}
+              </span>
+              <span className="sr-only">replies</span>
+            </button>
+          </span>
+        </div>
+      </div>
+    </article>
+  </li>
+);
+
+export default Comment;
---
+++ front_end/src/Components/CommentList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Comment from "./Comment";
+import { connect } from "react-redux";
+
+const CommentList = ({ activeVideo, isCommentsLoading }) => {
+  return !isCommentsLoading && activeVideo ? (
+    <ul className="space-y-3">
+      {activeVideo.comments.map((comment) => {
+        return <Comment comment={comment} />;
+      })}
+    </ul>
+  ) : (
+    <div>No Comments</div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  activeVideo: state.activeVideo,
+  isCommentsLoading: state.isCommentsLoading,
+});
+
+export default connect(mapStateToProps)(CommentList);
