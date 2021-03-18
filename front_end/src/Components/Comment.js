f09f98de75c9ed9557496b916424817b9077eb8a
DKlaver <dklaver15@gmail.com>
2021-03-18 15:20:04 -0500

    Added photo to be used as avatar.

--- front_end/src/Components/Comment.js
+++ front_end/src/Components/Comment.js
@@ -1,4 +1,5 @@
 import React from "react";
+import avatar from "../images/Brent_Solomon.png";
 
 const Comment = ({ comment }) => (
   <li className="list-none px-4 py-4 sm:px-6 rounded-lg bg-white shadow">
@@ -6,20 +7,7 @@ const Comment = ({ comment }) => (
       <div>
         <div className="flex space-x-3">
           <div className="flex-shrink-0">
-            <svg
-              className="h-10 w-10"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
+            <img src={avatar} alt="" width="50" height="50"></img>
           </div>
           <div className="min-w-0 flex-1">
             <p className="text-sm font-medium text-gray-900">Anonymous</p>
