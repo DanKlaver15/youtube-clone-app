3ddda825424165cdf666797989e005448035d262
DKlaver <dklaver15@gmail.com>
2021-03-18 20:37:41 -0500

    Removed test data and reconnected to youtube API.

--- front_end/src/videos/thunks.js
+++ front_end/src/videos/thunks.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { apiKey } from "../apiKey";
-import { videos } from "../testData";
 
 import {
   loadVideosInProgress,
@@ -12,11 +11,11 @@ export const loadVideos = (searchParam) => async (dispatch, getState) => {
   try {
     dispatch(loadVideosInProgress());
 
-    // const response = await axios.get(
-    //   `https://www.googleapis.com/youtube/v3/search?q=${searchParam}&key=${apiKey}&maxResults=20&part=snippet&type=video`
-    // );
+    const response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?q=${searchParam}&key=${apiKey}&maxResults=20&part=snippet&type=video`
+    );
 
-    // const videos = await response.data.items;
+    const videos = await response.data.items;
 
     dispatch(loadVideosInSuccess(videos));
   } catch (err) {
