affba88eab8323d625235a7200900430f023aedb
Lee Stratman <stratmanlc10@gmail.com>
2021-03-22 17:39:50 -0500

    Add 'loading' and 'no videos found' conditional in VideoList component.

--- front_end/src/Components/VideoList.js
+++ front_end/src/Components/VideoList.js
@@ -4,13 +4,15 @@ import { loadCommentsRequest } from "../comments/thunks";
 import { updateActiveVideo } from "../videos/actions";
 import VideoListItem from "./VideoListItem";
 
-const VideoList = ({ videos, onVideoSelect, onLoadComments }) => {
+const VideoList = ({ videos, isLoading, onVideoSelect, onLoadComments }) => {
   const onSelect = (video) => {
     onVideoSelect(video);
     onLoadComments(video.id.videoId);
   };
 
-  return (
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : videos.length > 0 ? (
     <ul className="divide-y divide-gray-200">
       {videos.map((video) => {
         return (
@@ -22,12 +24,14 @@ const VideoList = ({ videos, onVideoSelect, onLoadComments }) => {
         );
       })}
     </ul>
+  ) : (
+    <p>No videos found.</p>
   );
 };
 
 const mapStateToProps = (state) => ({
   videos: state.videos,
-  activeVideo: state.activeVideo,
+  isLoading: state.isLoading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
